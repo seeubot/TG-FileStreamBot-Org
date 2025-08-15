68b00649aa33a0342ebdc6620c3535423ea91141
seeubot <155578467+seeubot@users.noreply.github.com>
2025-08-15 09:27:06 +0530

    Update start.py

--- WebStreamer/plugins/start.py
+++ WebStreamer/plugins/start.py
@@ -1,14 +1,20 @@
 # This file is a part of TG-FileStreamBot
-# pylint: disable=relative-beyond-top-level
+#
+# Modifications made by Deekshith SH, 2024-2025
+# Copyright (C) 2024-2025 Deekshith SH
 
-from telethon.extensions import html
+import logging
+from telethon import Button
 from telethon.events import NewMessage
-from WebStreamer import __version__
 from WebStreamer.clients import StreamBot
 from WebStreamer.vars import Var
+from telethon.extensions import html
 
-@StreamBot.on(NewMessage(incoming=True,pattern=r"^\/start*", func=lambda e: e.is_private))
-async def start(event: NewMessage.Event):
+@StreamBot.on(NewMessage(pattern="/start", func=lambda e: e.is_private))
+async def start_command_handler(event: NewMessage.Event):
+    """
+    Handles the /start command. This is a basic diagnostic command to check if the bot is responsive.
+    """
     user = await event.get_sender()
     if (Var.ALLOWED_USERS and user.id not in Var.ALLOWED_USERS) or (
         Var.BLOCKED_USERS and user.id in Var.BLOCKED_USERS):
@@ -17,22 +23,28 @@ async def start(event: NewMessage.Event):
             link_preview=False,
             parse_mode=html
         )
+
+    # Log that the start command was received. This helps with debugging.
+    logging.info(f"Start command received from user ID: {user.id}")
+
+    # Respond with a welcome message and instructions.
+    welcome_message = (
+        "👋 Hello there! I'm your Telegram File Streamer Bot.\n\n"
+        "Just send or forward me a file, and I will generate a direct "
+        "streaming link for it.\n\n"
+        "Feel free to test my functionality! "
+    )
+
+    buttons = [[
+        Button.url("Support", url="https://t.me/your_support_channel"), # Replace with your support channel URL
+        Button.url("Source Code", url="https://github.com/your_github_repo") # Replace with your GitHub repo URL
+    ]]
+
     await event.message.reply(
-        message=f'Hi <a href="tg://user?id={user.id}">{user.first_name}</a>, Send me a file to get an instant stream link.',
+        message=welcome_message,
         link_preview=False,
+        buttons=buttons,
         parse_mode=html
     )
 
-@StreamBot.on(NewMessage(incoming=True,pattern=r"^\/about*", func=lambda e: e.is_private))
-async def about(event: NewMessage.Event):
-    await event.message.reply(
-        message=f"""
-Maintained By: <a href="https://github.com/DeekshithSH">DeekshithSH</a>
-Source Code: <a href="https://github.com/DeekshithSH/TG-FileStreamBot">TG-FileStreamBot</a>
-Based On: [<a href="https://github.com/tulir/TGFileStream/">tg filestream</a>] [<a href="https://github.com/EverythingSuckz/TG-FileStreamBot">TG-FileStreamBot</a>]
-Version: {__version__}
-Last Updated: 08 April 2025
-""",
-        link_preview=False,
-        parse_mode=html
-    )
+
