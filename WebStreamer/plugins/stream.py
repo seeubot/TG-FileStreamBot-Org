ea2d444cd6ca882d1912c612847f1a657935342c
seeubot <155578467+seeubot@users.noreply.github.com>
2025-08-15 12:21:28 +0530

    Update stream.py

--- WebStreamer/plugins/stream.py
+++ WebStreamer/plugins/stream.py
@@ -1,27 +1,19 @@
 # This file is a part of TG-FileStreamBot
-#
-# Modifications made by Deekshith SH, 2024-2025
-# Copyright (C) 2024-2025 Deekshith SH
+# pylint: disable=relative-beyond-top-level
 
 import logging
 from telethon import Button, errors
 from telethon.events import NewMessage
+from telethon.extensions import html
 from WebStreamer.clients import StreamBot
 from WebStreamer.utils.file_properties import get_file_info, pack_file, get_short_hash
 from WebStreamer.vars import Var
-from telethon.extensions import html
 
 MEDIA={"video", "audio"} # we can expand it to include more media types
 
-@StreamBot.on(NewMessage(func=lambda e: e.is_private and e.message.file, forwards=True))
+@StreamBot.on(NewMessage(func=lambda e: True if e.message.file and e.is_private else False))
 async def media_receive_handler(event: NewMessage.Event):
-    """
-    Handles incoming file messages in private chats and generates stream links.
-    """
     user = await event.get_sender()
-    
-    logging.info(f"Received a message from user ID: {user.id}")
-
     if (Var.ALLOWED_USERS and user.id not in Var.ALLOWED_USERS) or (
         Var.BLOCKED_USERS and user.id in Var.BLOCKED_USERS):
         return await event.message.reply(
@@ -29,45 +21,27 @@ async def media_receive_handler(event: NewMessage.Event):
             link_preview=False,
             parse_mode=html
         )
-    
-    if not event.message.file:
-        return await event.message.reply(
-            message="Please send me a file to get a stream link.",
-            link_preview=False,
-            parse_mode=html
-        )
-        
     try:
         log_msg=await event.message.forward_to(Var.BIN_CHANNEL)
-        
-        # FIX: Changed `event.client` to `StreamBot` to use the main bot instance
-        file_info=await get_file_info(StreamBot, log_msg.chat_id, log_msg.id)
-        
-        if not file_info:
-            logging.error("Failed to retrieve file info for message ID: %s", log_msg.id)
-            return await event.message.reply("Sorry, I could not get file info for this message.")
-
+        file_info=get_file_info(log_msg)
         full_hash = pack_file(
             file_info.file_name,
             file_info.file_size,
             file_info.mime_type,
-            file_info.file_id
+            file_info.id
         )
         file_hash=get_short_hash(full_hash)
         
         stream_link = f"{Var.URL}stream/{log_msg.id}?hash={file_hash}"
-        hls_stream_link = f"{Var.URL}hls/{log_msg.id}.m3u8?hash={file_hash}"
+        hls_link = f"{Var.URL}hls/{log_msg.id}.m3u8?hash={file_hash}"
         
         is_media = bool(set(file_info.mime_type.split("/")) & MEDIA)
-        
         buttons=[[Button.url("Open", url=stream_link)]]
         message = f"<code>{stream_link}</code>"
         
         if is_media:
-            buttons.append([Button.url("Stream", url=stream_link+"&s=1")])
-            buttons.append([Button.url("HLS Stream", url=hls_stream_link)])
-            message+=f"\n\n<a href='{stream_link}&s=1'>(Stream)</a>"
-            message+=f"\n<a href='{hls_stream_link}'>(HLS Stream)</a>"
+            buttons.append([Button.url("HLS Stream", url=hls_link)])
+            message+=f"\n\n<b>HLS Stream:</b> <code>{hls_link}</code>"
             
         await event.message.reply(
             message=message,
@@ -76,8 +50,5 @@ async def media_receive_handler(event: NewMessage.Event):
             parse_mode=html
         )
     except errors.FloodWaitError as e:
-        logging.error("FloodWaitError: %s", e)
-    except Exception as e:
-        logging.error("An unexpected error occurred: %s", e, exc_info=True)
-        await event.message.reply("An unexpected error occurred while processing your request. Please try again.")
+        logging.error(e)
 
